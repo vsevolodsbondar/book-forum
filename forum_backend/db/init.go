8fda853cb3ca26c56c89a99c295517beb38aeab1
Polina <polinafilippova314@gmail.com>
2026-09-09 12:22:41 +0300

    final fixes

--- forum_backend/db/init.go
+++ forum_backend/db/init.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Init() (*sql.DB, error) {
-	data, err := sql.Open("sqlite3", "./forum.db?_foreign_keys=on&_journal_mode=WAL&_busy_timeout=5000")
+	data, err := sql.Open("sqlite3", "./db/forum.db?_foreign_keys=on&_journal_mode=WAL&_busy_timeout=5000")
 	if err != nil {
 		return nil, err
 	}
@@ -22,7 +22,7 @@ func Init() (*sql.DB, error) {
 		data.Close()
 		return nil, err
 	}
-	//we need to decide what and how gonna print or log messages
+
 	fmt.Println("Connected to SQLite")
 	return data, nil
 }
--- forum_backend/migrations/0001_init_schema.sql
+++ forum_backend/migrations/0001_init_schema.sql
@@ -9,11 +9,11 @@ CREATE TABLE IF NOT EXISTS user(
     description TEXT
 );
 CREATE TABLE IF NOT EXISTS category(
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS post(
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     author_id INTEGER,
     category_id INTEGER,
@@ -24,27 +24,27 @@ CREATE TABLE IF NOT EXISTS post(
     FOREIGN KEY(category_id) REFERENCES category(id) ON DELETE SET NULL
 );
 CREATE TABLE IF NOT EXISTS comment(
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     text TEXT NOT NULL,
     created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
     post_id INTEGER NOT NULL,
     parent_comment_id INTEGER,
-    author_id INTEGER,
+    user_id INTEGER,
     FOREIGN KEY(post_id) REFERENCES post(id) ON DELETE CASCADE,
     FOREIGN KEY(parent_comment_id) REFERENCES comment(id) ON DELETE SET NULL,
-    FOREIGN KEY(author_id) REFERENCES user(id) ON DELETE SET NULL,
+    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE SET NULL,
     CHECK (parent_comment_id != id)
 );
 CREATE TABLE IF NOT EXISTS likes(
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     comment_id INTEGER NOT NULL,
-    type INTEGER NOT NULL,
+    type_of_like INTEGER NOT NULL,
     FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE,
     FOREIGN KEY(comment_id) REFERENCES comment(id) ON DELETE CASCADE,
     UNIQUE(user_id, comment_id),
-    CHECK(type IN (0,1))
+    CHECK(type_of_like IN (0,1))
 );
 CREATE INDEX idx_comment_post_id ON comment(post_id);
 CREATE INDEX idx_post_author_id ON post(author_id);
