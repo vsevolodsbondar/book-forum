bd264d97bcd17a0b25f5e5c8a30de26967b15c9f
Polina <polinafilippova314@gmail.com>
2026-09-09 10:12:44 +0300

    db init

--- forum_backend/go.mod
+++ forum_backend/go.mod
@@ -1,3 +1,5 @@
 module forum_backend
 
 go 1.26.0
+
+require github.com/mattn/go-sqlite3 v1.14.52 // indirect
--- forum_backend/main.go
+++ forum_backend/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"forum_backend/db"
 	"forum_backend/server"
 	"log"
 	"net/http"
 )
 
 func main() {
-
+	//initialize database
+	database, err := db.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
 	fmt.Println("Starting server...")
 	log.Fatalln(http.ListenAndServe(":8080", server.NewRouter()))
 }
