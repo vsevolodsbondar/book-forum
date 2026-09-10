61033e8eb3b82ba0ffb189e6250a0013a7e006fe
Polina <polinafilippova314@gmail.com>
2026-09-10 11:43:18 +0300

    get all api

--- forum_backend/model/comment.go
+++ forum_backend/model/comment.go
@@ -6,7 +6,7 @@ type Comment struct {
 	ID              int64     `json:"id"`
 	Text            string    `json:"text"`
 	PostID          int64     `json:"post_id"`
-	UserID          int64     `json:"user_id"`
+	UserID          *int64    `json:"user_id"`
 	ParentCommentID *int64    `json:"parent_comment_id"`
 	CreatedAt       time.Time `json:"created_at"`
 	UpdatedAt       time.Time `json:"updated_at"`
@@ -20,4 +20,10 @@ type CreateCommentRequest struct {
 type CreateCommentBody struct {
 	Text            string `json:"text"`
 	ParentCommentID *int64 `json:"parent_comment_id"`
+}
+type AllComments struct {
+	Comments []Comment
+	Page     int `json:"page"`
+	Size     int `json:"size"`
+	Total    int `json:"total"`
 }
