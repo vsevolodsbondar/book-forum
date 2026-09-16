88f34da2e9142cf6d0050dff7f090c90dc62d09d
vsevolodsbondar <vsevolod.s.bondar@gmail.com>
2026-09-16 15:32:25 +0300

    refactored

--- backend/helper/flag.go
+++ backend/helper/flag.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 )
 
-func FlagHandling() (bool, error) {
+func IsSeeding() bool {
 	initiate := flag.Bool("init", false, "adds premade data for db. To use it: --init")
 	flag.Parse()
 
-	return *initiate, nil
+	return *initiate
 }
