0a18db572cf9b53983a632db81a46f84772d8432
vsevolodsbondar <vsevolod.s.bondar@gmail.com>
2026-09-16 16:43:50 +0300

    fix: for some reason some refs didn't get to the merge. manually updated.

--- backend/db/init.go
+++ backend/db/init.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func Init(seeding bool) (*sql.DB, error) {
+func Init() (*sql.DB, error) {
 	data, err := sql.Open("sqlite3", "./db/forum.db?_foreign_keys=on&_journal_mode=WAL&_busy_timeout=5000")
 	if err != nil {
 		return nil, err
--- backend/main.go
+++ backend/main.go
@@ -4,24 +4,16 @@ import (
 	"context"
 	"fmt"
 	"forum_backend/db"
-	"forum_backend/helper"
 	"forum_backend/server"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	ctx := context.Background()
 
-	seeding, err := helper.FlagHandling()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		os.Exit(1)
-	}
-
 	//initialize database
-	database, err := db.Init(seeding)
+	database, err := db.Init()
 	if err != nil {
 		log.Fatal(err)
 	}
