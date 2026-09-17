5f5a4d0a780490c2e720bfac29218edde3c0fa0f
Polina <polinafilippova314@gmail.com>
2026-09-17 09:34:43 +0300

    rebase

--- backend/db/init.go
+++ backend/db/init.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func Init() (*sql.DB, error) {
+func Init(seeding bool) (*sql.DB, error) {
 	data, err := sql.Open("sqlite3", "./db/forum.db?_foreign_keys=on&_journal_mode=WAL&_busy_timeout=5000")
 	if err != nil {
 		return nil, err
@@ -24,11 +24,16 @@ func Init() (*sql.DB, error) {
 		return nil, err
 	}
 
+<<<<<<< HEAD
 	if helper.IsSeeding() {
 		if err := SeedDB(data); err != nil {
 			data.Close()
 			return nil, err
 		}
+=======
+	if seeding {
+		SeedDB(data)
+>>>>>>> 6e22c57 (now seeding with a flag.)
 	}
 
 	fmt.Println("Connected to SQLite")
--- backend/helper/flag.go
+++ backend/helper/flag.go
@@ -4,9 +4,17 @@ import (
 	"flag"
 )
 
+<<<<<<< HEAD
 func IsSeeding() bool {
 	initiate := flag.Bool("init", false, "adds premade data for db. To use it: --init")
 	flag.Parse()
 
 	return *initiate
+=======
+func FlagHandling() (bool, error) {
+	initiate := flag.Bool("init", false, "adds premade data for db. To use it: --init")
+	flag.Parse()
+
+	return *initiate, nil
+>>>>>>> 6e22c57 (now seeding with a flag.)
 }
