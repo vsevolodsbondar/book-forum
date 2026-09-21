69fa92660d78dc07a20940794b8048670faca5b2
Polina <polinafilippova314@gmail.com>
2026-09-21 14:25:28 +0300

    fixes

--- frontend/src/static/scripts/header.js
+++ frontend/src/static/scripts/header.js
@@ -6,7 +6,7 @@ function renderLoggedOutHeader(){
             <a href="login.html#login" class="shadow_btn header_login">Login</a>`;
 };
 function renderLoggedInHeader(){
-   container.ineerHTML = `<a href="createPost.html" aria-label="link to the page create post" class="main_btn">Write a post</a>
+   container.innerHTML = `<a href="createPost.html" aria-label="link to the page create post" class="main_btn">Write a post</a>
             <div class="image-container">
                 <img class="author_img" src="./src/static/imgs/photo.jpeg" alt="photo of user">
             </div>
