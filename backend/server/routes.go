26e8a0cbaf39f29cfc137fa875c62ca1e645d99a
Polina <polinafilippova314@gmail.com>
2026-09-21 14:48:27 +0300

    integrated to auth

--- backend/server/routes.go
+++ backend/server/routes.go
@@ -1,14 +1,15 @@
 package server
 
 import (
+	"forum_backend/custom_err"
 	"forum_backend/handler"
 	"net/http"
 )
 
 func RegisterRoutes(mux *http.ServeMux, comments *handler.CommentHandler) {
 	mux.HandleFunc("GET /api/posts", handler.GetLanding)
-	mux.HandleFunc("GET /posts/{id}/comments", comments.GetAllByPostID)
-	mux.HandleFunc("POST /posts/{id}/comments", comments.Create)
-	mux.HandleFunc("PATCH /comments/{id}", comments.Update)
-	mux.HandleFunc("DELETE /comments/{id}", comments.Delete)
+	mux.HandleFunc("GET /posts/{id}/comments", custom_err.GlobalErrorHandler(comments.GetAllByPostID))
+	mux.HandleFunc("POST /posts/{id}/comments", custom_err.GlobalErrorHandler(comments.Create))
+	mux.HandleFunc("PATCH /comments/{id}", custom_err.GlobalErrorHandler(comments.Update))
+	mux.HandleFunc("DELETE /comments/{id}", custom_err.GlobalErrorHandler(comments.Delete))
 }
