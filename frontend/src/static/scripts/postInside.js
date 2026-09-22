9b09454af7a7b39287dc518fa8604f3816aee297
Polina <polinafilippova314@gmail.com>
2026-09-22 11:48:17 +0300

    inittial page of comments

--- frontend/src/static/scripts/postInside.js
+++ frontend/src/static/scripts/postInside.js
@@ -1,19 +1,87 @@
 //api to post by id
 const params = new URLSearchParams(window.location.search);
 const postID = params.get("id");
-async function GetPostData(){
+async function getPostData(){
     try{
         const response = await fetch(`http://localhost:8080/posts/${postID}/comments`);
         if (!response.ok){
             throw new Error(`Error HTTP: ${response.status}`); 
         };
         const postData = await response.json();
+        renderPostData(postData);
         console.log(postData);
     } catch(error){
         console.log("the error was catched", error);
     }
 };
-GetPostData();
+getPostData();
+
+//render post and comments
+function renderPostData(data){
+    const postDiscription = document.querySelector(".comment.init")
+    postDiscription.innerHTML = `
+    <div class="author">
+                    <div class="image-container">
+                        <img class="author_img" src="/src/static/imgs/default.jpg" alt="photo of ${data.comments[0].user_for_comment.username}">
+                    </div>
+                    <div class="author-info">
+                        <p class="username">${data.comments[0].user_for_comment.username}</p>
+                        <p class="date">${formatDate(data.comments[0].updated_at)}</p>
+                    </div>
+                </div>
+                <h3 class="post_title">${data.title}</h3>
+                <p class="description">${data.comments[0].text}</p>
+                <div class="post-footer">
+                    <div class="tags">
+                        <div class="tags_item">${data.category}</div>
+                    </div>
+                    <div class="stats">
+                        <button class="likes">${data.comments[0].likes}</button>
+                        <button class="dislikes">${data.comments[0].dislikes}</button>
+                    </div>
+                </div>`;
+                renderAllComments(data);
+};
+function renderAllComments(data){
+    const container = document.querySelector(".comment-container");
+    for (let i=1; i<data.comments.length; i++){
+        const name = data.comments[i].user_for_comment.username;
+        const date = formatDate(data.comments[i].updated_at);
+        const text = data.comments[i].text;
+        const likes = data.comments[i].likes;
+        const dislikes = data.comments[i].dislikes;
+        const id = data.comments[i].id;
+        const newEl = `
+        <article id="${id}" class="comment">
+                <div class="author">
+                    <div class="image-container">
+                        <img class="author_img" src="/src/static/imgs/default.jpg" alt="photo of ${name}">
+                    </div>
+                    <div class="author-info">
+                        <p class="username">${name}</p>
+                        <p class="date">${date}</p>
+                    </div>
+                </div>
+                <p class="description">${text}</p>
+                <div class="post-footer">
+                    <div class="stats">
+                        <button class="likes">${likes}</button>
+                        <button class="dislikes">${dislikes}</button>
+                    </div>
+                    <div class="reply">
+                        <button class="blue_empty_btn btn_update">Edit</button>
+                        <button class="blue_empty_btn btn_reply">Reply</button>
+                    </div>
+                </div>
+            </article>`;
+            container.insertAdjacentHTML('beforeend', newEl);
+    };
+};
+//formating of the date
+function formatDate(isoString){
+    const date = new Date(isoString);
+    return date.toLocaleDateString(undefined, { day: 'numeric', month: 'long', year: 'numeric' });
+};
 
 // click events on update button
 const postInside = document.querySelector(".post-inside");
