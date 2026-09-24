e052d3ae92b31267a28f52bf4977113dd1936d1f
Polina <polinafilippova314@gmail.com>
2026-09-24 12:36:33 +0300

    untested render of header and nav

--- frontend/src/static/scripts/header.js
+++ frontend/src/static/scripts/header.js
@@ -1,12 +1,24 @@
 //
-function getCookies(name){
-    const cookies = document.cookie.split("; ");
-    for (const cookie of cookies) {
-        const [key, value] = cookie.split("=");
-        if (key === name) return value;
-    };
-    return null;
+async function checkIsUserLoggedIn(){
+        try{
+        const response = await fetch(`http://localhost:8080/me`);
+        if (!response.ok){
+            renderLoggedOutHeader();
+            return;
+        };
+        const userID = await response.json();
+        const responseUser = await fetch(`http://localhost:8080/users/${userID}`);
+        if (!responseUser.ok){
+            throw new Error(`Error HTTP: ${response.status}`); 
+        };
+        const userData = await response.json();
+        renderLoggedInHeader(userData);
+        renderLoggedInNav(userData);
+    } catch(error){
+        console.log("the error was catched", error);
+    }
 };
+checkIsUserLoggedIn();
 //render of header
 const header = document.querySelector("header.header");
 const container = document.querySelector(".header .header-btn");
@@ -15,11 +27,18 @@ function renderLoggedOutHeader(){
             <a href="login.html#reg" class="main_btn header_register">Register</a>
             <a href="login.html#login" class="shadow_btn header_login">Login</a>`;
 };
-function renderLoggedInHeader(){
+function renderLoggedInHeader(user){
    container.innerHTML = `<a href="createPost.html" aria-label="link to the page create post" class="main_btn">Write a post</a>
             <div class="image-container">
-                <img class="author_img" src="./src/static/imgs/photo.jpeg" alt="photo of user">
+                <img class="author_img" src="./src/static/imgs/default.jpg" alt="photo of ${user.username}">
             </div>
             `;
 };
-renderLoggedOutHeader();
+//render navigation
+const list = document.querySelector(".nav-list")
+function renderLoggedInNav(user){
+    let newEl;
+    newEl.innerHTML = `<li><a class="nav-list_item" href="createPost.html">Create post</a></li>
+    <li><a class="nav-list_item" href="myPosts?id=${user.id}.html">My posts</a></li>`; 
+    list.insertAdjacentHTML("beforeend", newEl);
+}
