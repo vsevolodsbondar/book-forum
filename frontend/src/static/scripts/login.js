ba923a980c7fff0bc997bf793baac13cd450f715
Polina <polinafilippova314@gmail.com>
2026-09-26 10:46:38 +0300

    added biome and made some fixes

--- frontend/src/static/scripts/login.js
+++ frontend/src/static/scripts/login.js
@@ -1,19 +1,19 @@
 //change of file name when image was chosen from computer
-const input = document.getElementById('file-field');
-const label = document.querySelector('.file-field_label');
+const input = document.getElementById("file-field");
+const label = document.querySelector(".file-field_label");
 
-input.addEventListener("change", function() {
-  if (this.files && this.files.length > 0) {
-    label.textContent = this.files[0].name;
-  }
+input.addEventListener("change", function () {
+	if (this.files && this.files.length > 0) {
+		label.textContent = this.files[0].name;
+	}
 });
 //click on register and login button
 const regBtn = document.getElementById("toRegister");
 const loginBtn = document.getElementById("toLogin");
 const formContainer = document.querySelector(".form-container");
-regBtn.addEventListener("click", function(){
-    formContainer.style.justifyContent = "flex-end";
+regBtn.addEventListener("click", () => {
+	formContainer.style.justifyContent = "flex-end";
 });
-loginBtn.addEventListener("click", function(){
-    formContainer.style.justifyContent = "flex-start";
+loginBtn.addEventListener("click", () => {
+	formContainer.style.justifyContent = "flex-start";
 });
